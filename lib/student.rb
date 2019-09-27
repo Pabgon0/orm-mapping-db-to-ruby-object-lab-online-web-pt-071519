84ae4d61b2a6c5ceb01e575cfd5ccbcc1d90befe
Pablo Gonzalez <pabgon0@gmail.com>
2019-09-27 21:35:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -69,7 +69,7 @@ class Student
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       array = self.new_from_db(row)
       array
     end.first
