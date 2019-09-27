9bd0b50ed6db58c0589e5d007cd31c4dc22a824f
Pablo Gonzalez <pabgon0@gmail.com>
2019-09-27 21:33:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,20 @@ class Student
     end
   end
   
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      array = self.new_from_db(row)
+      array
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
